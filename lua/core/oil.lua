88473cc5d7b879427f9d31e3a1adfcfe2c0309c1
Jade Piramide <jadepiramidepogi@gmail.com>
2024-05-19 02:35:10 -0700

    Add detailed view in oil

--- lua/core/oil.lua
+++ lua/core/oil.lua
@@ -8,6 +8,22 @@ return {
           default_file_exporer = true,
           columns = { "icon" },
 
+          keymaps = {
+            ["gd"] = { 
+              desc = "Toggle detailed view",
+              callback = function()
+                local oil = require("oil")
+                local config = require("oil.config")
+
+                if #config.columns == 1 then
+                  oil.set_columns({ "icon", "permissions", "size", "mtime" })
+                else
+                  oil.set_columns({ "icon" })
+                end
+              end
+            },
+          },
+
           view_options = {
             show_hidden = true,
           },
