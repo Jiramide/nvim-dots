456fc9133eff83ceb8c69c318ae1bd9c230e6311
Jade Piramide <jadepiramidepogi@gmail.com>
2024-05-30 19:32:57 -0700

    Change oil to always hide ..

--- lua/core/oil.lua
+++ lua/core/oil.lua
@@ -26,6 +26,10 @@ return {
 
           view_options = {
             show_hidden = true,
+
+            is_always_hidden = function(name, _)
+              return name == ".."
+            end,
           },
         }
       )
